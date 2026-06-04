c09c78607cca185f5b960dcd05719bb20e59a405
Yuri França <yurisamuca65@gmail.com>
2026-06-04 17:20:23 -0300

    [ADD] Player completo, Jogo Infinito e tutorial
    - Plataformas spawnam outras e se destroem
    - Player morre
    - Jogo reinicia
    - Animações do player

---
+++ Sapo Pulão/Assets/Scripts/Chao_Script.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public class Chao_Script : MonoBehaviour
+{
+    bool PlayerChegou = false;
+    bool PossoDestruir = false;
+    Vector3 Offset;
+    float tempoDestruir = 2f;
+    public GameObject SelfOBJ;
+    public Vector2 Min_Max_Distance;
+
+    void Start()
+    {
+        Offset = Vector3.right*Random.Range(Min_Max_Distance[0], Min_Max_Distance[1]);
+    }
+
+    void Update()
+    {
+        if (PossoDestruir)
+        {
+            tempoDestruir -= Time.deltaTime;
+            if(tempoDestruir <= 0)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+    }
+    void OnCollisionEnter2D(Collision2D cool)
+    {
+        if(cool.gameObject.name == "SapoOBJ")
+        {
+            if (!PlayerChegou)
+            {
+               Instantiate(SelfOBJ, this.gameObject.transform.position+Offset, this.gameObject.transform.rotation);
+                PlayerChegou = true; 
+            }
+            else
+            {
+                PossoDestruir = false;
+                tempoDestruir = 2f;
+            }
+        }
+    }
+    void OnCollisionExit2D(Collision2D cool)
+    {
+        if(cool.gameObject.name == "SapoOBJ")
+        {
+            PossoDestruir = true;
+        }
+    }
+}
--- Sapo Pulão/Assets/Scripts/PlayerMoveset_Script.cs
+++ Sapo Pulão/Assets/Scripts/PlayerMoveset_Script.cs
@@ -2,13 +2,16 @@ using System;
 using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 public class PlayerMoveset_Script : MonoBehaviour
 {
     [SerializeField] float H_Force;
     [SerializeField] Vector2 Força_AtualMAX;
     [SerializeField] Vector3 Offset;
+    [SerializeField] Animator animeSapo;
     public Transform CameraFoco;
     public Rigidbody2D fisicaSapo;
+    public float TimeDelayJump;
     bool ReleasedSpace = true;
     void Start()
     {
@@ -22,11 +25,17 @@ public class PlayerMoveset_Script : MonoBehaviour
         if (Keyboard.current.spaceKey.wasPressedThisFrame)
         {
             Debug.Log("Apertei :)");
+            if(Força_AtualMAX[0] == 0 && !ReleasedSpace)
+            {
+                animeSapo.speed = 1/(TimeDelayJump*Força_AtualMAX[1]);
+                animeSapo.Play("Carregando_Pulo");
+            }
             StartCoroutine(ChangeForce());
         }
         if (Keyboard.current.spaceKey.wasReleasedThisFrame)
         {
             Debug.Log("Soltei :(");
+            animeSapo.speed = 1;
             if(Força_AtualMAX[0] != 0)
             {
                 //INTERROMPER LOOP
@@ -36,6 +45,7 @@ public class PlayerMoveset_Script : MonoBehaviour
                 //PULAR
                 Debug.Log("Pulei com força: " + Força_AtualMAX[0].ToString());
                 fisicaSapo.AddForce(new Vector2(H_Force, Força_AtualMAX[0]*2), ForceMode2D.Impulse);
+                animeSapo.Play("PulandoA");
 
                 //ZERAR FORÇA
                 Força_AtualMAX[0] = 0;
@@ -49,10 +59,16 @@ public class PlayerMoveset_Script : MonoBehaviour
         {
             Debug.Log("No chao");
             ReleasedSpace = false;
+            animeSapo.Play("PulandoB");
 
             //MUDAR FOCO DA CAMERA
             CameraFoco.position = colisao.gameObject.transform.position+Offset;
         }
+        if(colisao.gameObject.tag == "Respawn")
+        {
+            SceneManager.LoadScene(0);
+        }
+        Debug.Log(colisao.gameObject.name);
     }
 
     IEnumerator ChangeForce()
@@ -61,7 +77,7 @@ public class PlayerMoveset_Script : MonoBehaviour
         {
             Debug.Log("Mudou");
             Força_AtualMAX[0] = Math.Clamp(Força_AtualMAX[0]+1, 0, Força_AtualMAX[1]);
-            yield return new WaitForSeconds(0.5f);
+            yield return new WaitForSeconds(TimeDelayJump);
         }
     }
 }
